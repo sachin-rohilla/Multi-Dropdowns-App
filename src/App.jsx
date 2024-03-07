de983babad5ea91ace900c74776413558401999d
sachin-rohilla <sachinrohilla23799@gmail.com>
2024-03-07 17:43:33 +0530

    fix-code

--- src/App.jsx
+++ src/App.jsx
@@ -12,20 +12,34 @@ function App() {
   const [countryId, setCountryId] = useState(null);
   const [stateId, setStateId] = useState(null);
   const [cityId, setCityId] = useState(null);
-  const handleChange = (e) => {
+  const handleCountryChange = (e) => {
     setCountryId(e.target.value);
+    setStateId(null);
+    setCityId(null);
+  };
+  const handleStateChange = (e) => {
     setStateId(e.target.value);
+    setCityId(null);
+  };
+  const handleCityChange = (e) => {
     setCityId(e.target.value);
   };
+  const handlePincodeChange = (e) => {
+    console.log(e.target.value);
+  };
+
   return (
     <>
       <div className="w-full flex flex-col items-center gap-4 mt-10">
         <h1 className="text-xl lg:text-3xl font-semibold">Multi Dropdown </h1>
         <div className="flex flex-col lg:flex-row gap-4  ">
-          <CountryDropdown handleChange={handleChange} />
-          <StateDropdown handleChange={handleChange} countryId={countryId} />
-          <CityDropdown handleChange={handleChange} stateId={stateId} />
-          <PincodeDropdown handleChange={handleChange} cityId={cityId} />
+          <CountryDropdown handleChange={handleCountryChange} />
+          <StateDropdown
+            handleChange={handleStateChange}
+            countryId={countryId}
+          />
+          <CityDropdown handleChange={handleCityChange} stateId={stateId} />
+          <PincodeDropdown handleChange={handlePincodeChange} cityId={cityId} />
         </div>
       </div>
     </>
